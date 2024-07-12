2cfac2b73c272edb5b6a5bb08152b8216b8b360b
1Mashilo <christophermashilo@gmail.com>
2024-07-12 14:14:27 +0200

    improved documentation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
+import asyncio
+from typing import List
+from 0x01-python_async_function.0-main import wait_random
+
 """
 Module for concurrent coroutines
 """
-import asyncio
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Waits for random delays and returns a list of the resulting delays.
 
@@ -17,9 +19,9 @@ async def wait_n(n: int, max_delay: int) -> float:
         max_delay (int): The maximum delay value for each call to wait_random.
 
     Returns:
-        float: A list of the resulting delays.
+        List[float]: A list of the resulting delays.
 
     """
     tasks = [wait_random(max_delay) for i in range(n)]
     delays = await asyncio.gather(*tasks)
-    return delays
+    return sorted(delays)
