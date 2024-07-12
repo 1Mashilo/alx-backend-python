8673bd8d5d326e659ebd60217930d5eaf99610c5
1Mashilo <christophermashilo@gmail.com>
2024-07-12 15:14:29 +0200

    PEP8 validation

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 """
-This module contains a function to measure the runtime of asynchronous coroutines.
+This module contains a function to measure
+the runtime of asynchronous coroutines.
 
 Functions:
-    measure_time: Measures the average runtime of a specified number of coroutines.
+    measure_time: Measures the average runtime
+    of a specified number of coroutines.
 """
 
 import asyncio
 import time
-from typing import List
-from concurrent_coroutines import wait_n
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
 
 async def measure_time(n: int, max_delay: int) -> float:
     """
-    Measures the total execution time for wait_n(n, max_delay) and returns the average time per coroutine.
+    Measures the total execution time for wait_n(n, max_delay)
+    and returns the average time per coroutine.
 
     Args:
         n (int): The number of coroutines to spawn.
