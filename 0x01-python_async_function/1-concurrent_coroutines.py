623955fc28ef29d3a38a7815ee2f48ee32d1a55f
1Mashilo <christophermashilo@gmail.com>
2024-07-12 14:18:46 +0200

    PEP8 validation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-import asyncio
-from typing import List
 """
 Module for concurrent coroutines
 """
+import asyncio
+from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
