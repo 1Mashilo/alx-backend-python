7154ef3b2f0c2a15ea8a17ef8c78703e32591873
1Mashilo <christophermashilo@gmail.com>
2024-07-29 13:53:10 +0200

    🔧 fix NameError

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,10 +5,10 @@ import unittest
 from typing import Dict, Tuple, Union
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-
 from utils import (
     access_nested_map,
     get_json,
+    memoize,
 )
 
 
