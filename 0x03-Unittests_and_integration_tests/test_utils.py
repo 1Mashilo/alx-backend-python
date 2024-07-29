d1f3346d6c66ed69528e18b464784598b1fd085e
1Mashilo <christophermashilo@gmail.com>
2024-07-29 12:30:58 +0200

    ✅ Complete task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python3
-"""A module for testing the utils module.
-"""
 import unittest
 from typing import Dict, Tuple, Union
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 
+#!/usr/bin/env python3
+"""A module for testing the utils module.
+"""
+
 from utils import (
     access_nested_map,
     get_json,
-    memoize,
 )
 
 
@@ -67,3 +67,33 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test case class for testing the `get_json` function.
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(
+            self,
+            test_url: str,
+            test_payload: Dict,
+            ) -> None:
+        """
+        Test method for testing the expected output of the `get_json` function.
+
+        Args:
+            test_url (str): The URL to fetch JSON data from.
+            test_payload (Dict): The expected JSON payload.
+
+        Returns:
+            None
+        """
+        attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
